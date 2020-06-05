c10d227fb71a743d5d72a1fbb46ae4701fd36325
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-06-05 11:56:08 -0400

    commit

---
+++ src/main/java/com/simpleJwt/server/SimpleJwtServer/SimpleJwtServerApplication.java
@@ -0,0 +1,13 @@
+package com.simpleJwt.server.SimpleJwtServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJwtServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJwtServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpleJwt/server/SimpleJwtServer/SimpleJwtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleJwt.server.SimpleJwtServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJwtServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
